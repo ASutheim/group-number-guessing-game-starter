4aee543b06fb7ffcf562a7fa71cdeb10a345be66
Anna Sutheim <asutheim@gmail.com>
2023-05-11 20:45:30 -0500

    Notes in each file for functions

--- server/server.js
+++ server/server.js
@@ -14,3 +14,31 @@ app.use(express.static("server/public"));
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
+
+let randomNumber;
+let guessArray = []
+
+
+function generateNumber() {
+  randomNumber = Math.floor(Math.random() * 25) + 1;
+  console.log("Here's a random number between 1 and 25:", randomNumber);
+  return randomNumber;
+}
+
+function compareGuesses() {
+  //this function needs to take the submitted guesses from the client
+  //(passed as an array), and compare each guess to the value of RandomNumber.
+
+  for (guess of GuessArray) {
+    if (guess > randomNumber) {
+      //code which tells client js to update DOM with '${guess} is too high, try again
+    }
+    else if (guess < randomNumber) {
+      //code which tells client.js to update DOM with '${guess} is too low, try again'
+    }
+    else if (guess === randomNumber) {
+      //code which tells client.js to update DOM with "YOU GOT IT!" and turn background green
+      //also to empty out the guessArray
+  }
+}
+}
